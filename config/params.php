e29a52a94c2a0188d8e03141510cdfaaba4d2c3d
Serov Roman <info@inter-projects.ru>
2019-09-30 22:12:35 +0300

    Fix config

--- config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'reCaptcha' => [
-        'siteKey' => '6Le7argUAAAAAIqUTR9GKfwcsPeJJB2UcKu_-vFs',
-        'secret'  => '6Le7argUAAAAANddqH6LGJRqfe7mwxgAU-MucwEt',
-    ],
-];
