b32eab4ce4c9ba28436aaffa1160fd0b77b87e2d
Serov Roman <info@inter-projects.ru>
2019-10-31 23:07:20 +0300

    Labels

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -843,7 +843,7 @@ class ShopController extends AbstractController
         $searchtext = Yii::$app->request->get('searchtext', false);
         $searchtag  = Yii::$app->request->get('tag', false);
 
-        $limit = 5;
+        $limit = 20;
         $product_page = Yii::$app->request->get('page', 1);
         $offset = ($product_page - 1) * $limit;
         $product_count = 0;
