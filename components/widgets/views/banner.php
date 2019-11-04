27114b80ba6e406da9a3e198690e7854548df57e
Serov Roman <info@inter-projects.ru>
2019-11-05 00:21:47 +0300

    Banner style

--- components/widgets/views/banner.php
+++ components/widgets/views/banner.php
@@ -9,10 +9,10 @@ use yii\helpers\Html;
 
 ?>
 
-<div id="main-banners-carousel" class="carousel slide main_banners" data-ride="carousel">
+<div id="main-banners-carousel" class="carousel slide main_banners hidden-xs hidden-sm" data-ride="carousel">
      <?php if (count($banners) > 1) { ?>
     <!-- Indicators -->
-    <ol class="carousel-indicators hidden-xs">
+    <ol class="carousel-indicators">
         <?php for ($s = 0; $s < count($banners); $s++) { ?>
         <li data-target="#main-banners-carousel" data-slide-to="<?= $s ?>"<?= !$s ? ' class="active"' : '' ?>></li>
         <?php } ?>
@@ -34,8 +34,31 @@ use yii\helpers\Html;
 
     <?php if (count($banners) > 1) { ?>
     <!-- Controls -->
-    <a class="left carousel-control hidden-xs" href="#main-banners-carousel" role="button" data-slide="prev"></a>
-    <a class="right carousel-control hidden-xs" href="#main-banners-carousel" role="button" data-slide="next"></a>
+    <a class="left carousel-control" href="#main-banners-carousel" role="button" data-slide="prev"></a>
+    <a class="right carousel-control" href="#main-banners-carousel" role="button" data-slide="next"></a>
     <?php } ?>
 </div>
+
+</div>
+</div>
+
+<div id="main-banners-carousel_mobile" class="carousel slide main_banners hidden-lg hidden-md" data-ride="carousel">
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <?php for ($s = 0; $s < count($banners); $s++) { ?>
+        <<?= $banners[$s]->link ? 'a href="'.$banners[$s]->link.'"' : 'div' ?> class="item<?= !$s ? ' active' : '' ?>">
+            <div class="banner_caption">
+                <?= $banners[$s]->banner_text ?>
+            </div>
+            <?= file_exists($banners[$s]->uploadFolder.'/'.$banners[$s]->id.'.jpg') ? Html::img($banners[$s]->getPreview($banners[$s]->uploadFolder.'/'.$banners[$s]->id.'.jpg', 1440, 500, true)) : '' ?>
+            <div class="carousel-caption"></div>
+        </<?= $banners[$s]->link ? 'a' : 'div' ?>>
+        <?php } ?>
+    </div>
+</div>
+
+<div class="container content_block">
+    <div class="container_content_block">
+
+
 
